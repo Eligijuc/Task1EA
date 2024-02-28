d97cbcb5ddfc0b57e4a19b84d55d8dfbc3a760e3
Eligijuc <eligijus.abromikas@gmail.com>
2024-02-28 13:52:51 +0200

    FinTech task 1, first button

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvmain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.tvmain = findViewById(R.id.tvmain);
+    }
+
+    public void ChangeMainTxt(View view) {
+        this.tvmain.setText("The World Is Mine!");
+    }
+}
